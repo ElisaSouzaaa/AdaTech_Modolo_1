4ad96a7ec48cc50148dec7f13da3988b37e8d1d0
Elisa Souza <102561113+ElisaSouzaaa@users.noreply.github.com>
2023-12-13 19:25:56 -0300

    Update Calcular.java

--- lista03/src/ex_05/Calcular.java
+++ lista03/src/ex_05/Calcular.java
@@ -2,8 +2,8 @@ package ex_05;
 
 public class Calcular {
     public double calcularLitrosCombustivel(double tempo, double velocidadeMedia) {
-        double distancia = tempo * velocidadeMedia; // Calcula a distância percorrida (distância = tempo * velocidade)
-        double litrosNecessarios = distancia / 12.0; // Calcula a quantidade de litros necessários (litros = distância / consumo)
+        double distancia = tempo * velocidadeMedia;
+        double litrosNecessarios = distancia / 12.0;
         return litrosNecessarios;
     }
 }
